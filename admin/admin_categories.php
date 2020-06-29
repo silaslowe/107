98d5c2f66839298392989654665748434abfe96f
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-06-29 17:04:27 -0500

    first commit

---
+++ admin/admin_categories.php
@@ -0,0 +1,92 @@
+<?php include "includes/admin_header.php"; ?>
+
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        
+        <?php include "includes/admin_nav.php"; ?> 
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>                       
+
+<div class="col-xs-6">
+
+<?php insert_categories(); ?>
+
+<form action="" method="post">
+  <div class="form-group">
+  <label for="cat_title">Add Category</label>
+  <input class="form-control" type="text" name="cat_title">
+</div>
+<div class="form-group">
+  <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+</div>
+</form>
+
+<?php //UPDATE AND INCLUDE QUERY
+
+if(isset($_GET['edit'])) {
+
+$cat_id = $_GET['edit'];
+
+include "includes/admin_categories_update.php";
+
+}
+
+?>
+
+</div> 
+<div class="col-xs-6">
+
+<?php
+$query = "SELECT * FROM categories";  
+$select_categories = mysqli_query($connection, $query);
+
+?>
+
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Category Title</th>
+        </tr>
+    </thead>
+    <tbody>
+
+
+<?php //FIND ALL CATEGORIES QUERY
+
+findAllCategories()
+
+?>
+
+
+<?php //DELETE QUERY
+
+deleteCategories();
+ 
+?>
+
+</tbody>
+
+</div>
+
+
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+<?php include "includes/admin_footer.php"; ?>
---
+++ includes/db.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "myObj:php$$$12";
+$db['db_name'] = "CMS";
+
+foreach($db as $key => $value) {
+
+define(strtoupper($key), $value);
+
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+?>
+
+
